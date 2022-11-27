06ef0234631927b6b13b340d9f22df25d7a52efc
Anastasia Silyutina <ASilyutina@slb.com>
2022-11-27 09:36:17 +0300

    second  try

--- Work/Program.cs
+++ Work/Program.cs
@@ -2,61 +2,27 @@
 // длина которых меньше либо равна 3 символа.
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнений алгоритма.
 
-
-Console.WriteLine("Cozdadim massiv is neskolkix znachenii \n kakoe kolichestvo znachenii  budem vyvodit?");
-int countArrayValue = int.Parse(Console.ReadLine());
-
-string [] startArray = new string [countArrayValue];
- 
- string [] FillArray (string [] array)
- {
-	for (int i = 0; i < countArrayValue; i++)
-	{
-		Console.WriteLine($"Vvedite {i}  znachenie:");
-		array[i] = Console.ReadLine();
-	}
-	return array;
-}
-
-string [] SortArray (string [] array)
+string[] array1 = new string[5] {"hi", "@", "test", ":)", "test2"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
-	int countNecessaryValue = 0;
-	string arrayValue = string.Empty;
-	for (int i = 0; i < array.Length; i++)
-	{
-		arrayValue = array[i];
-		if (arrayValue.Length <= 3) countNecessaryValue++;
-	}
-
-	string [] endArray = new string [countNecessaryValue];
-	int index = 0;
-	for (int i = 0; i < array.Length; i++)
-	{
-		arrayValue = array[i];
-		if (arrayValue.Length <= 3)
-		{
-			endArray[index] = array[i];
-			index++;
-		}
-	}
-	return endArray;
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
 }
-
-void PrintArray (string [] array)
+void PrintArray(string[] array)
 {
-	if (array.Length == 0) Console.WriteLine("Massiv ne imeet znachenii!");
-		for (int i = 0; i < array.Length; i++)
-		{
-			Console.Write($"  [{array[i]}]");
-		}
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
-
-FillArray(startArray);
-
-string [] endArray = SortArray(startArray);
-
-Console.WriteLine("\n Pervonachalnyi massiv:");
-PrintArray(startArray);
-
-Console.WriteLine("\n  Massiv znachenii c dlinoi (<) libo = (3) simvola:");
-PrintArray(endArray);
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
